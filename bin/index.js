df4cbd146b7ec44277f116ac2f47c8d632a696b2
07krW5Hnr5ghy <jryn145@gmail.com>
2025-01-03 23:26:12 -0500

    feature(main): fetch git hub api user activity data

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,40 @@
 #!/usr/bin/env node
+const https = require('https');
 // CLI handle
 const main = () => {
     const [,,username] = process.argv;
     if(username){
-        console.log("ok");
-        console.log(username);
+        const options = {
+            host:'api.github.com',
+            path:`/users/${username}/events/public`,
+            method:'GET',
+            headers:{
+                'Accept':'application/vnd.github+json',
+                'User-Agent': 'Mozilla/5.0'
+            },
+        };
+        let request = https.request(options,(res)=>{
+            if(res.statusCode!==200){
+                console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
+                res.resume();
+                return;
+            }
+
+            let data = '';
+
+            res.on('data',(chunk)=>{
+                data += chunk;
+            });
+
+            res.on('close',()=>{
+                console.log('Retrieved all data');
+                console.log(JSON.parse(data));
+            });
+        });
+        request.end();
+        request.on('error',(err)=>{
+            console.error(`Encountered an error trying trying to make a request : ${err.message}`);
+        });
     }else{
         console.log(`
             Usage: 
